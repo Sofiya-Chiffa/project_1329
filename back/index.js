1d973130e3cb67ce50958a8501abb4b5734732cf
Sofiya-Chiffa <94921852+Sofiya-Chiffa@users.noreply.github.com>
2022-12-22 01:19:52 +0300

    Update index.js

--- back/index.js
+++ back/index.js
@@ -20,29 +20,28 @@ app.get("/", async (req, res) => {
   });
 
 app.get("/menu", async (req, res) => {
-    const data = req.query.data;
-    const result = await db.all("SELECT * FROM menu WHERE day = " + data);
+    const day = req.query.day;
+    const result = await db.all("SELECT * FROM menu WHERE day = " + day);
     res.send(result);
   });
 
 app.get("/order", async (req, res) => {
     const data = req.query.data;
     const place = req.query.place;
-    const result = await db.all("SELECT * FROM orders WHERE data = " + data + "AND place = " + place);
+    const result = await db.all("SELECT * FROM orders WHERE date = " + data + " AND place = " + place);
     res.send(result);
   });
 
 app.get("/orders", async (req, res) => {
     const data = req.query.data;
-    const place = req.query.place;
-    const result = await db.all("SELECT * FROM orders WHERE data = " + data + "AND place = " + place);
+    const result = await db.all("SELECT * FROM orders WHERE date = " + data);
     res.send(result);
   });
 
 app.get("/meal", async (req, res) => {
     const time = req.query.time;
     const num = req.query.num;
-    const result = await db.all("SELECT * FROM orders WHERE foodtime = " + time + "AND number = " + num);
+    const result = await db.all("SELECT * FROM meals WHERE foodtime = " + time + " AND number = " + num);
     res.send(result);
   });
 
