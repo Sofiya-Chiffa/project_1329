3788e6d0acabcacc1c708f559642e4c478be0f8b
Sofiya-Chiffa <94921852+Sofiya-Chiffa@users.noreply.github.com>
2022-12-22 01:19:11 +0300

    Update exp.js

--- my-app/src/pages/services/exp.js
+++ my-app/src/pages/services/exp.js
@@ -4,19 +4,22 @@ export const getTest = () => {
 };
 
 export const getMenu = (time) => {
-  return () => {
-    console.log("asd");
-    return fetch(`http://localhost:5000/menu?data=${time}`).then((e) =>
-      e.json()
-    );
+    return () => {
+    return fetch(`http://localhost:5000/menu?day=${time}`).then((e) =>
+      e.json());
   };
 };
 
 
 export const getMeal = (time, num) => {
-  return fetch(`http://localhost:5000/order?time=${time}&num=${num}`).then((e) => e.json());
+  console.log(time, num);
+  return fetch(`http://localhost:5000/meal?time=${time}&num=${num}`).then((e) => e.json());
 };
 
 export const getOrder = (data, place) => {
   return fetch(`http://localhost:5000/order?data=${data}&place=${place}`).then((e) => e.json());
+};
+
+export const getOrders = (data) => {
+  return fetch(`http://localhost:5000/orders?data=${data}`).then((e) => e.json());
 };
